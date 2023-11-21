62b3fdb0055ef08421bb6708c81f73d6f27087e5
ZeekHoft <109581639+ZeekHoft@users.noreply.github.com>
2023-11-21 20:32:44 +0800

    Update api_homepage.py

--- api_homepage.py
+++ api_homepage.py
@@ -1,5 +1,11 @@
 from flask import  Flask, render_template
 import sqlite3
+from waitress import serve
+from paste.translogger import TransLogger
+import logging
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.INFO)
+
 
 app = Flask(__name__, template_folder=r'C:\Users\DLTPO\PycharmProjects\pythonProject1\emergency_app\database_emer_file\emer_app_api_temp')
 
@@ -24,4 +30,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  serve(TransLogger(app, setup_console_handler=False), host='127.0.0.1', port=8080)
